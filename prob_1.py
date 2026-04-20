4ed6bee6a4fedae07c57e8df881860d80500acb6
kedar hatte <kedarhatte2425@gmail.com>
2026-04-20 20:05:59 +0530

    Add files via upload

---
+++ prob_1.py
@@ -0,0 +1,23 @@
+a = "kedar"
+print(type(a))
+
+b = 31
+print(type(b))
+
+c = 3.14
+print(type(c))
+
+d = True
+print(type(d))
+
+e = False
+print(type(e))
+
+f = [1,2,3]
+print(type(f))
+
+g = (1,2,3,4)
+print(type(g))
+
+h = {1,2,3,4}
+print(type(h))
